a29995ed3e67caa35caa608c956404aad6083703
ACADEMY-74648DJ\Administrator <Andreas.Antonsson@collectorbank.se>
2018-03-19 10:14:53 +0100

    Fixing Attributes logic

--- MainProgram/Attributes.cs
+++ MainProgram/Attributes.cs
@@ -3,13 +3,14 @@
     public class Attributes
     {
         public int strengt, intellect, agility;
+
         public static bool operator< (Attributes attributes, Attributes other)
         {
             return attributes.strengt < other.strengt && attributes.intellect < other.intellect && attributes.agility < other.agility ;
         }
         public static bool operator> (Attributes attributes, Attributes other)
         {
-            return false;
+            return attributes.strengt > other.strengt && attributes.intellect > other.intellect && attributes.agility > other.agility;
         }
     }
 }
